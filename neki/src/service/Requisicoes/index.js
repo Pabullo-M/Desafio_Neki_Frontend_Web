12146b183c898c9bd0f50699400ad942dc29a693
Pabullo-M <pabullo.muniz@gmail.com>
2024-08-07 09:48:57 -0300

    feat: cria metodos get e post para Skills e UsuarioSikll

--- neki/src/service/Requisicoes/index.js
+++ neki/src/service/Requisicoes/index.js
@@ -11,31 +11,79 @@ const api = Api();
 //         console.error(error);
 //     });
 
-    export const postLogin=(usuario, senha)=>{
-        api.post('/usuario/login', { 
-            usuario,
-            senha
-         })
-            .then(response => {
-                console.log(response.data.id);
-                saveData('id',response.data.id)
-                console.log(response.data.token)
-                saveData('token',response.data.token)
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }
+export const getAllSkills = ()=>{
+    api.get('/Skills')
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
 
-    export const postCadastroUsuario=(usuario, senha)=>{
-        api.post('/usuario/cadastro', { 
-            usuario,
-            senha
-         })
-            .then(response => {
-                console.log(response.data);
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }
+}
+export const getUsuarioSkills = (usuarioId)=>{
+    api.get(`/usuarioSkill/${usuarioId}`)
+        .then(response => {
+            console.log(response.data);
+            response.data;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+}
+
+export const DeleteUsuarioSkill = (skillId)=>{
+    api.delete(`/usuarioSkill/${skillId}`)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+}
+
+
+export const postCadastroSkills=(usuarioId, skillId, level)=>{
+    api.post('/usuarioSkill/add', { 
+        usuarioId,
+        skillId,
+        level
+        })
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+export const postLogin=(usuario, senha)=>{
+    api.post('/usuario/login', { 
+        usuario,
+        senha
+        })
+        .then(response => {
+            console.log(response.data.id);
+            saveData('id',response.data.id)
+            console.log(response.data.token)
+            saveData('token',response.data.token)
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+export const postCadastroUsuario=(usuario, senha)=>{
+    api.post('/usuario/cadastro', { 
+        usuario,
+        senha
+        })
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
